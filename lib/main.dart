92cc23248f2f2562d577930e637bbefbd99ea416
recent <recentnafimahmed@gmail.com>
2023-06-17 21:13:06 +0600

    splash page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:careturor/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
  main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,22 +17,56 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blueGrey,
-      ),
-      home:  Home(),
+    return Sizer(
+      builder: (context, orientation, deviceType){
+
+        return GetMaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blueGrey,
+          ),
+          home: AnimatedSplashScreen(
+            duration: 1000,
+            splash: Scaffold(
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: 50.w,
+                        height: 50.h,
+                        child: Image.asset('assets/download.png',height: 70.h,width: 70.w,),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            nextScreen: Home(),
+            splashTransition: SplashTransition.fadeTransition,
+            splashIconSize: double.maxFinite,
+            backgroundColor:  Color(0xFFFFF8F8),
+          ), //Home(),
+        );
+      },
+
     );
+
+
   }
 }
 
